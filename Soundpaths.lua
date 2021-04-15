f9b290999a3b6a4df10c1ca5b7288d734625bb78
Aurae <anchastain@gmail.com>
2021-04-15 15:54:29 -0500

    Initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,26 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFED00FFGurgthock - Pig|r", [[Interface\Addons\SharedMedia_Inversion\sound\GurgthockPig.ogg]])
+LSM:Register("sound", "|cFFED00FFButtons - Mrraaazz!|r", [[Interface\Addons\SharedMedia_Inversion\sound\ButtonsMrraaazz.ogg]])
+LSM:Register("sound", "|cFFED00FFVe'nari - Stygia|r", [[Interface\Addons\SharedMedia_Inversion\sound\Ve'nariStygia.ogg]])
+LSM:Register("sound", "|cFFED00FFVe'nari - Turn Back|r", [[Interface\Addons\SharedMedia_Inversion\sound\Ve'nariTurnBack.ogg]])
+LSM:Register("sound", "|cFFED00FFArtificer Xymox - Die Together|r", [[Interface\Addons\SharedMedia_Inversion\sound\ArtificerXymoxDieTogether.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
