b3ae649a965a977f9c1e10760090002727cb6559
Aurae <anchastain@gmail.com>
2021-04-27 19:35:00 -0500

    New Sound
    Ion's Skill Issue quote added.

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -20,7 +20,7 @@ LSM:Register("sound", "|cFFED00FFButtons - Mrraaazz!|r", [[Interface\Addons\Shar
 LSM:Register("sound", "|cFFED00FFVe'nari - Stygia|r", [[Interface\Addons\SharedMedia_Inversion\sound\Ve'nariStygia.ogg]])
 LSM:Register("sound", "|cFFED00FFVe'nari - Turn Back|r", [[Interface\Addons\SharedMedia_Inversion\sound\Ve'nariTurnBack.ogg]])
 LSM:Register("sound", "|cFFED00FFArtificer Xymox - Die Together|r", [[Interface\Addons\SharedMedia_Inversion\sound\ArtificerXymoxDieTogether.ogg]])
-
+LSM:Register("sound", "|cFFED00FFIon Hazzikostas - Skill Issue|r", [[Interface\Addons\SharedMedia_Inversion\sound\IonSkillIssue.ogg]])
 -- -----
 --   STATUSBAR
 -- -----
